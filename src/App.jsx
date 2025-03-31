c2dfbf1eb6ecf2b212bf58eb27898a02155f2cdd
Caroline-An <anacarolinesn@outlook.com>
2025-03-31 20:30:55 -0300

    criando conteúdo do componente AddTask

--- src/App.jsx
+++ src/App.jsx
@@ -44,14 +44,24 @@ function App() {
     setTask(newTasks);
   }
 
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: tasks.length + 1,
+      text: title,
+      description: description,
+      isCompleted: false,
+    };
+    setTask([...tasks, newTask]);
+  }
+
   return (
     //dentro dos returns só pode entrar uma div por vez
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
-        <h1 className="text-3xl text-slate-100 font-bold font-sans">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold font-sans text-center">
           Gerenciador de Tarefas
         </h1>
-        <AddTask />
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks
           tasks={tasks}
           onTaskClick={onTaskClick}
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,34 @@
-function AddTask() {
-  return <h1>Add Task</h1>;
+//1,15,49
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        value={title}
+        type="text"
+        placeholder="Digite o título da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        value={description}
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => onAddTaskSubmit(title, description)}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
 }
 
 export default AddTask;
