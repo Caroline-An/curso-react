67ef1e3a478b7d05d3942a3b682e8f0fe736830c
Caroline-An <anacarolinesn@outlook.com>
2025-04-03 23:49:18 -0300

    adicionando um localStorage para persistir os dados no navegador

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import { v4 } from "uuid";
 
 function App() {
   //usamos o State quando ao alterar a variável precisamos alterar a interface também
-  const [tasks, setTask] = useState([
-    {
-      id: 1,
-      title: "Estudar programação",
-      description: "15 de julho às 10:00",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Estudar matemática",
-      description: "16 de julho às 10:00",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Estudar português",
-      description: "17 de julho às 10:00",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTask] = useState(
+    JSON.parse(localStorage.getItem("tasks")) ||
+      [
+        /*{
+        id: 1,
+        title: "Estudar programação",
+        description: "15 de julho às 10:00",
+        isCompleted: false,
+      },
+      {
+        id: 2,
+        title: "Estudar matemática",
+        description: "16 de julho às 10:00",
+        isCompleted: false,
+      },
+      {
+        id: 3,
+        title: "Estudar português",
+        description: "17 de julho às 10:00",
+        isCompleted: false,
+      },*/
+      ]
+  );
+
+  //executa a função {} sempre que algum valor que for adicionado na lista [] for atualizado
+  useEffect(() => {
+    //dessa forma estou convertendo o task para string
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
@@ -47,7 +56,7 @@ function App() {
 
   function onAddTaskSubmit(title, description) {
     const newTask = {
-      id: v4, //v4 cria um id aleatório
+      id: v4(), //v4 cria um id aleatório
       title: title,
       description: description,
       isCompleted: false,
