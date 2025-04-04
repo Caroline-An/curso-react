8dbd63406102a6e99f2a07ce8b4152501adf722e
Caroline-An <anacarolinesn@outlook.com>
2025-04-04 20:19:08 -0300

    adicionando botãozinho  na task concluída

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, DeleteIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -14,10 +14,11 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`text-left bg-slate-400 w-full text-white p-2 rounded-md ${
+            className={`flex items-center gap-2 text-left bg-slate-400 w-full text-white p-2 rounded-md ${
               task.isCompleted && "line-through"
             }`}
           >
+            {task.isCompleted && <CheckIcon />}
             {task.title}
           </button>
           <Button onClick={() => onSeeDetailsClick(task)}>
